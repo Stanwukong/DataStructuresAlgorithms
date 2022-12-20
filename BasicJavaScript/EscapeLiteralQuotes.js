1d488dac1900f9caf9397664b7c06aebdf29f098
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-12-20 15:11:07 +0100

    Basic JavaScript | 25%

---
+++ BasicJavaScript/EscapeLiteralQuotes.js
@@ -0,0 +1,5 @@
+let myStr = ""; 
+// Change below this line
+// Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see
+// I am a "double quoted" string inside "double quotes".
+myStr = "I am a \"double quoted\" string inside \"double quotes\".";
