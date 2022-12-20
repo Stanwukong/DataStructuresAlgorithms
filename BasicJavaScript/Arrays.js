22a2baee82fb854818a48b10ea9beb1d28648d3a
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-12-20 16:50:42 +0100

    JavaScript Basics | 35%

---
+++ BasicJavaScript/Arrays.js
@@ -0,0 +1,3 @@
+// Modify the new array myArray so that it contains both a string and a number (in that order).
+// Only change code below this line
+const myArray = ["First", 2];
---
+++ BasicJavaScript/NthCharacter.js
@@ -0,0 +1,6 @@
+// Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
