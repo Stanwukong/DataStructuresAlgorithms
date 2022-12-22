a776173348a0213b714c3d7a2ab6eaee924da0e8
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-12-22 06:14:58 +0100

    Basic JavaScript | 51%

---
+++ BasicJavaScript/BooleanValues.js
@@ -0,0 +1,8 @@
+// Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+function welcomeToBooleans() {
+	// Only change code below this line
+  
+	return true; // Change this line
+  
+	// Only change code above this line
+  }
