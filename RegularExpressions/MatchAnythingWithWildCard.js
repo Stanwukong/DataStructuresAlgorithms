5060ce812ee658dd88d7bd44780691422bb246b2
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-12-30 09:57:15 +0100

    Regular Expressions | 21%

---
+++ RegularExpressions/MatchAnythingWithWildCard.js
@@ -0,0 +1,8 @@
+/**
+ * Complete the regex unRegex so that it matches
+ * the strings run, sun, fun, pun, nun, and bun.
+ * Your regex should use the wildcard character.
+*/
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
