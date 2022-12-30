d081c924fba06ec7af3f05a0ebff4c3dbf5518f6
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-12-30 11:10:45 +0100

    Regular Expressions | 30%

---
+++ RegularExpressions/MatchAlphabets.js
@@ -0,0 +1,9 @@
+/**
+ * Match all the letters in the string quoteSample
+ * 
+ * Note: Be sure to match both uppercase and
+ * lowercase letters.
+*/
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
