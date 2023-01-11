d19002f20847f2d78e3ed91f453898da5a58257b
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-01-11 07:03:33 +0100

    Functional Programming | 65%

---
+++ FunctionalProgramming/AddElementsWithConcatInsteadOfPush.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/functional-programming/add-elements-to-the-end-of-an-array-using-concat-instead-of-push
+function nonMutatingPush(original, newItem) {
+	// Only change code below this line
+	return original.concat(newItem);
+  
+	// Only change code above this line
+  }
+  
+  const first = [1, 2, 3];
+  const second = [4, 5];
+  nonMutatingPush(first, second);
